a94717cf9b48fb8d305c9f3e5fe7786be7cccfa8
Aman <aman_A00@mail.ru>
2020-06-13 19:20:49 +0600

    initial commit with main settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router,  Route, Switch} from "react-router-dom";
 
-function App() {
+type OwnProps = {}
+const App: React.FC<OwnProps> = (props) => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Router>
+            <Switch>
+                <Route exact path={'/'}>
+
+                </Route>
+            </Switch>
+        </Router>
     </div>
   );
 }
 
-export default App;
+export default App
