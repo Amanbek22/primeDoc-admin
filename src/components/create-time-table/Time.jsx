8a89e3aee62ab0516a292472f4478a778934ee12
Aman <aman_A00@mail.ru>
2020-07-14 16:12:59 +0600

    timetable layout 50%

--- src/components/create-time-table/Time.jsx
+++ src/components/create-time-table/Time.jsx
@@ -1,50 +1,97 @@
 import React from 'react'
 import css from './create-time.module.css'
 import del from '../../img/delete.png'
-import {DatePicker} from "@y0c/react-datepicker";
 import 'dayjs/locale/ru'
 import './date.scss'
+import Select from 'react-select'
+
+const colourStyles = {
+    control: (styles) => ({
+        ...styles,
+        background: '#F8F8F8',
+        width: '120px',
+        border: '1px solid rgba(0, 0, 0, 0.1)',
+        borderRadius: '5px 0 0 5px',
+        padding: '3px 5px',
+        marginTop: '50px',
+
+    }),
+    // option: (styles:any) => {
+    //     return {...styles,width:'350px'}
+    // }
+};
+const colourStyles2 = {
+    control: (styles) => ({
+        ...styles,
+        background: '#F8F8F8',
+        width: '120px',
+        border: '1px solid rgba(0, 0, 0, 0.1)',
+        borderRadius: '0 5px 5px 0',
+        padding: '3px 5px',
+        marginTop: '50px',
+
+    }),
+    // option: (styles:any) => {
+    //     return {...styles,width:'350px'}
+    // }
+};
 
 const Time = () => {
-    // const [start, setStart] = useState(null)
-    const clocks = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23']
+    const clocks = [
+        {value: '00', label: '00'},
+        {value: '01', label: '01'},
+        {value: '02', label: '02'},
+        {value: '03', label: '03'},
+        {value: '04', label: '04'},
+        {value: '05', label: '05'},
+        {value: '06', label: '06'},
+        {value: '07', label: '07'},
+        {value: '08', label: '08'},
+        {value: '09', label: '09'},
+        {value: '10', label: '10'},
+        {value: '11', label: '11'},
+        {value: '12', label: '12'},
+        {value: '13', label: '13'},
+        {value: '14', label: '14'},
+        {value: '15', label: '15'},
+        {value: '16', label: '16'},
+        {value: '17', label: '17'},
+        {value: '18', label: '18'},
+        {value: '19', label: '19'},
+        {value: '20', label: '20'},
+        {value: '21', label: '21'},
+        {value: '22', label: '22'},
+        {value: '23', label: '23'},
+        ]
+    const minutes = [
+        {value: '00', label: '00'},
+        {value: '05', label: '05'},
+        {value: '10', label: '10'},
+        {value: '15', label: '15'},
+        {value: '20', label: '20'},
+        {value: '25', label: '25'},
+        {value: '30', label: '30'},
+        {value: '35', label: '35'},
+        {value: '40', label: '40'},
+        {value: '45', label: '45'},
+        {value: '50', label: '50'},
+        {value: '55', label: '55'},
+    ]
     return (
         <>
             <div className={css.timeWrapper}>
-                <label>
+                <span>
                     <span>От</span>
-                    <select className={css.timePicker} autoFocus={true}>
-                        <option value="">Часы</option>
-                        {
-                            clocks.map((item) => {
-                                return <option value={item} key={item}>{item}</option>
-                            })
-                        }
-                    </select>
-                    <select className={css.minPicker}>
-                        <option value="">Минуты</option>
-                        <option value="00">00</option>
-                        <option value="30">30</option>
-                    </select>
-                </label>
+                    <Select options={clocks} placeholder={'Часы'} styles={colourStyles}/>
+                    <Select options={minutes} placeholder={'Минуты'} styles={colourStyles2}/>
+                </span>
 
-                <label>
+                <span>
                     <span>До</span>
-                    <select className={css.timePicker} autoFocus={true}>
-                        <option value="">Часы</option>
-                        {
-                            clocks.map((item) => {
-                                return <option value={item} key={item}>{item}</option>
-                            })
-                        }
-                    </select>
-                    <select className={css.minPicker}>
-                        <option value="">Минуты</option>
-                        <option value="00">00</option>
-                        <option value="30">30</option>
-                    </select>
-                </label>
-                <img src={del} alt="#"/>
+                    <Select options={clocks} placeholder={'Часы'} styles={colourStyles}/>
+                    <Select options={minutes} placeholder={'Минуты'} styles={colourStyles2}/>
+                </span>
+                <img className={css.del} src={del} alt="#"/>
             </div>
         </>
     )
@@ -57,14 +104,14 @@ export const Date = () => {
     const days = ['Понедельник','Вторник','Среда','Четверг','Пятница','Суббота','Воскресенье']
     return (
         <>
-            {/*<select className={css.datePicker}>*/}
-            {/*    {*/}
-            {/*        days.map((item) => {*/}
-            {/*            return <option value={item} key={item}>{item}</option>*/}
-            {/*        })*/}
-            {/*    }*/}
-            {/*</select>*/}
-            <DatePicker locale={'ru'} multiple onChange={(e) => console.log(e)}/>
+            <select className={css.datePicker}>
+                {
+                    days.map((item) => {
+                        return <option value={item} key={item}>{item}</option>
+                    })
+                }
+            </select>
+            {/*<DatePicker locale={'ru'} multiple onChange={(e) => console.log(e)}/>*/}
         </>
     )
 }
