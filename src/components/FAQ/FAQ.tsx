b4d9d38dc4b29da4f9a36789717706bafb0860f3
Aman <aman_A00@mail.ru>
2020-07-16 22:58:03 +0600

    drag and drop 50%

--- src/components/FAQ/FAQ.tsx
+++ src/components/FAQ/FAQ.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from 'react'
+import React, {SetStateAction, useEffect, useRef, useState} from 'react'
 import {useDispatch} from "react-redux";
 import {setHeader} from "../../state/appReducer";
 import edit from "../../img/edit.png";
@@ -8,6 +8,15 @@ import css from './faq.module.css'
 import api from "../../api/Api";
 import Preloader from "../preloader/Preloader";
 import {useFormik} from "formik";
+import {
+    GridContextProvider,
+    GridDropZone,
+    GridItem,
+    swap
+} from "react-grid-dnd";
+import DeleteModal from "../utils/DeleteModal";
+import ModalWrapper from "../modal/Modal";
+import EditDeleteComponent from "../utils/EditDelete";
 
 
 const validate = (values: any) => {
@@ -24,12 +33,6 @@ const validate = (values: any) => {
         errors.answer = 'Required'
     }
 
-    // if (!values.email) {
-    //     errors.email = 'Required';
-    // } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    //     errors.email = 'Invalid email address';
-    // }
-
     return errors;
 };
 
@@ -38,17 +41,31 @@ const Faq = React.memo(() => {
     const [visible, setVisible] = useState(false)
     const [pending, setPending] = useState(true)
     const [questions, setQuestions] = useState([])
+    const [change, setChange] = useState(false)
     useEffect(() => {
         dispatch(setHeader("FAQ"))
     }, [dispatch])
+    console.log(questions)
+
+    function compare(a: any, b: any) {
+        const bandA = a.order
+        const bandB = b.order
+        let comparison = 0;
+        if (bandA > bandB) {
+            comparison = 1;
+        } else if (bandA < bandB) {
+            comparison = -1;
+        }
+        return comparison;
+    }
 
     useEffect(() => {
         api.getFaq().then((res: any) => {
-            setQuestions(res.data)
+            const data = res.data.sort(compare)
+            setQuestions(data)
             setPending(false)
         }, (error: any) => console.error(error))
     }, [])
-
     const formik = useFormik({
         initialValues: {
             question: "",
@@ -60,7 +77,8 @@ const Faq = React.memo(() => {
             api.createFaq(values)
                 .then((res: any) => {
                     api.getFaq().then((res: any) => {
-                        setQuestions(res.data)
+                        const data = res.data.sort(compare)
+                        setQuestions(data)
                         setVisible(false)
                         values.question = ''
                         values.answer = ''
@@ -69,14 +87,51 @@ const Faq = React.memo(() => {
         }
     })
 
+    function onChange(sourceId: any, sourceIndex: any, targetIndex: any, targetId: any) {
+        const nextState = swap(questions, sourceIndex, targetIndex);
+        const newArr = nextState.map((item: any, index: number) => {
+            return {
+                ...item,
+                order: index + 1
+            }
+        })
+        // @ts-ignore
+        setQuestions(newArr);
+    }
+
     if (pending) {
         return <Preloader/>
     }
 
     return (
         <div className={css.wrapper}>
-            {questions.map((item: any) => <List answer={item.answer} id={item.id} questions={item.question}
-                                                key={item.id}/>)}
+            <BtnFloat>
+                {
+                    change
+                        ? <GreenBtn onClick={() => setChange(!change)}>Сохранить</GreenBtn>
+                        : <GreenBtn onClick={() => setChange(!change)}>Изменить порядок</GreenBtn>
+                }
+            </BtnFloat>
+            {
+                !change
+                    ? questions.map((item: any) => <List answer={item.answer} id={item.id} questions={item.question}
+                                                         key={item.id}/>)
+                    : <GridContextProvider onChange={onChange}>
+                        <GridDropZone
+                            id="items"
+                            boxesPerRow={1}
+                            rowHeight={100}
+                            className={css.gridDropZone}
+                            style={{height: "400px"}}
+                        >
+                            {questions.map((item: any) => <GridItem className={css.q} key={item.id}>
+                                    <List noItem={true} noClick={true} answer={item.answer} id={item.id}
+                                          questions={item.question} key={item.id}/>
+                                </GridItem>
+                            )}
+                        </GridDropZone>
+                    </GridContextProvider>
+            }
             <BtnFloat>
                 <GreenBtn onClick={() => {
 
@@ -141,39 +196,53 @@ type ListProps = {
     questions: string
     answer: string
     id: number
+    noItem?: boolean
+    noClick?: boolean
 }
 const List: React.FC<ListProps> = (props) => {
-    const [visible, setVisible] = useState(false)
+    const [visible1, setVisible1] = useState(false)
     const innerRef = useOuterClick((e: any) => {
         setVisible(false)
     });
-    const onDelete = (id: number | string) => {
-        api.deleteFaq(id)
+    const onDelete = () => {
+        api.deleteFaq(props.id)
             .then((res: any) => {
-
+                    console.log(res)
                 },
                 (error: any) => console.error(error)
             )
     }
+    const [visible, setVisible] = useState(false)
+    const [editing, setEditing] = useState(false)
+    const onModal = () => setVisible(!visible)
+    const onEdit = () => setEditing(!editing)
+    const setFaq = () => {
+        onEdit()
+    }
     return (
         // @ts-ignore
         <div ref={innerRef} className={css.questionWrapper}>
-            <div onClick={() => setVisible(!visible)} className={css.question}>
+            <div
+                onClick={() => props.noClick ? null : setVisible(!visible)}
+                className={css.question}>
                 <span className={css.q}>{props.questions}</span>
                 <div>
                     {
-                        visible
-                            ? <div className={css.answer + ' ' + visible ? css.animate : ''}>{props.answer}</div>
+                        visible1
+                            ? <div className={css.answer + ' ' + visible1 ? css.animate : ''}>{props.answer}</div>
                             : null
                     }
                 </div>
             </div>
             <span>
-                <EditDelete>
-                        <img src={edit} alt="edit"/>
-                        <img onClick={() => onDelete(props.id)} src={del} alt="delete"/>
-                </EditDelete>
+                {
+                    props.noItem ? null :
+                        <EditDeleteComponent editing={editing} onEdit={onEdit} onModal={onModal} onDone={setFaq} />
+                }
             </span>
+            <ModalWrapper onModal={onModal} visible={visible} width={"450"} height={"400"} onClickAway={onModal}>
+                <DeleteModal text={'Вы уверены что хотите удалить'} onModal={onModal} title={props.questions} del={onDelete}/>
+            </ModalWrapper>
         </div>
     )
 }
